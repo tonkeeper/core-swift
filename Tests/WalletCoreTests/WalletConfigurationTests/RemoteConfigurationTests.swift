41051afebe09b9e246c727d68bff804004be7dd2
Grigory Serebryanyy <grishamsc@gmail.com>
2023-06-20 11:06:49 +0200

    Configuration load
    - add LoadConfigurationRequest
    - add LoadConfigurationRequest test

--- Tests/WalletCoreTests/WalletConfigurationTests/RemoteConfigurationTests.swift
+++ Tests/WalletCoreTests/WalletConfigurationTests/RemoteConfigurationTests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+import TonAPI
 @testable import WalletCore
 
 final class RemoteConfigurationTests: XCTestCase {
@@ -54,5 +55,27 @@ final class RemoteConfigurationTests: XCTestCase {
         
         let decoder = JSONDecoder()
         XCTAssertNoThrow(try decoder.decode(RemoteConfiguration.self, from: configurationResponseString.data(using: .utf8)!))
+    }
+    
+    func testLoadConfigurationRequest() throws {
+        // GIVEN
+        let requestBuilder = URLRequestBuilder()
+        let baseURL = URL(string: "https://tonkeeper.io")!
+        let lang = "en"
+        let build = "9.9.9"
+        let chainName = "testnet"
+        let platform = "macos"
+        let apiRequest = LoadConfigurationRequest(
+            lang: lang,
+            build: build,
+            chainName: chainName,
+            platform: platform)
+        let urlString = "\(baseURL.absoluteString)/keys?lang=\(lang)&build=\(build)&chainName=\(chainName)&platform=\(platform)"
+        
+        // WHEN
+        let urlRequest = try requestBuilder.build(with: apiRequest.request, baseURL: baseURL)
+        
+        // THEN
+        XCTAssertEqual(urlRequest.url!.absoluteString, urlString)
     }
 }
