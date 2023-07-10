3ea0c91b2a87b877b25a7f24b1728411ee239c20
Grigory Serebryanyy <grishamsc@gmail.com>
2023-07-10 17:35:04 +0200

    Formatting
    - update DecimalFormatter to calculate fractional digits count

--- Source/WalletCore/Formatters/DecimalAmountFormatter.swift
+++ Source/WalletCore/Formatters/DecimalAmountFormatter.swift
@@ -15,10 +15,31 @@ struct DecimalAmountFormatter {
         self.numberFormatter = numberFormatter
     }
     
-    func format(amount: Decimal, symbol: String?) -> String {
+    func format(amount: Decimal, symbol: String?, maximumFractionDigits: Int? = nil) -> String {
+        let formatterMaximumFractionDigits: Int
+        if let maximumFractionDigits = maximumFractionDigits {
+            formatterMaximumFractionDigits = maximumFractionDigits
+        } else {
+            formatterMaximumFractionDigits = calculateFractionalDigitsCount(amount: amount, maximumNotZeroFractionalCount: 2)
+        }
+        let formatFractional = String(repeating: "#", count: formatterMaximumFractionDigits)
         let decimalNumberAmount = NSDecimalNumber(decimal: amount)
         numberFormatter.currencySymbol = symbol
-        numberFormatter.positiveFormat = "¤ #,##0.00"
+        numberFormatter.positiveFormat = "¤ #,##0.\(formatFractional)"
         return numberFormatter.string(from: decimalNumberAmount) ?? ""
+    }
+}
+
+private extension DecimalAmountFormatter {
+    func calculateFractionalDigitsCount(amount: Decimal,
+                                        maximumNotZeroFractionalCount: Int) -> Int {
+        let amountNumber = NSDecimalNumber(decimal: amount)
+        let amountFractionalLength = max(Int16(-amount.exponent), 0)
+        let amountFractional = amountNumber
+            .subtracting(NSDecimalNumber(integerLiteral: amountNumber.intValue))
+            .multiplying(byPowerOf10: amountFractionalLength)
+        let notZeroFractionalCount = String(amountFractional.intValue).count
+        let formatterFractinalDigitsCount = Int(amountFractionalLength) - notZeroFractionalCount + min(maximumNotZeroFractionalCount, notZeroFractionalCount)
+        return formatterFractinalDigitsCount
     }
 }
