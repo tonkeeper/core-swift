435258060439c585f749547ab57a25e221f05e93
Grigory Serebryanyy <grishamsc@gmail.com>
2023-09-30 18:16:33 +0200

    Balance widget
    - add Error to BalanceWidgetController

--- Source/WalletCore/Widgets/BalanceWidgetController.swift
+++ Source/WalletCore/Widgets/BalanceWidgetController.swift
@@ -9,6 +9,11 @@ import Foundation
 import BigInt
 
 public final class BalanceWidgetController {
+    public enum Error: Swift.Error {
+        case failedToLoad
+        case noWallet
+    }
+    
     public struct Model {
         public let tonBalance: String
         public let fiatBalance: String
@@ -37,37 +42,47 @@ public final class BalanceWidgetController {
     }
     
     public func loadBalance() async throws -> Model {
-        let wallet = try walletProvider.activeWallet
-        let contract = try WalletContractBuilder().walletContract(with: try wallet.publicKey, contractVersion: wallet.contractVersion)
-        async let balanceTask = balanceService.loadWalletBalance(wallet: wallet)
-        async let ratesTask = ratesService.loadRates(tonInfo: TonInfo(),
-                                                     tokens: [],
-                                                     currencies: [.USD])
-        
-        let balance = try await balanceTask
-        let formattedFiatBalance: String
-        if let rates = try? await ratesTask,
-           let tonUSDRate = rates.ton.first(where: { $0.currency == .USD }) {
-            let fiatAmount = RateConverter().convert(
-                amount: balance.tonBalance.amount.quantity,
-                amountFractionLength: balance.tonBalance.amount.tonInfo.fractionDigits,
-                rate: tonUSDRate
-            )
-            formattedFiatBalance = amountFormatter.formatAmountWithoutFractionIfThousand(
-                fiatAmount.amount,
-                fractionDigits: fiatAmount.fractionLength,
-                maximumFractionDigits: 2,
-                symbol: Currency.USD.symbol
-            )
-        } else {
-            formattedFiatBalance = "\(Currency.USD.symbol ?? "")-----"
+        guard let wallet = try? walletProvider.activeWallet else {
+            throw Error.noWallet
         }
-        let formattedBalance = amountFormatter.formatAmount(
-            BigInt(integerLiteral: balance.tonBalance.amount.quantity),
-            fractionDigits: balance.tonBalance.amount.tonInfo.fractionDigits,
-            maximumFractionDigits: 2
-        )
         
-        return Model(tonBalance: formattedBalance, fiatBalance: formattedFiatBalance, address: try contract.address().shortString)
+        do {
+            let contract = try WalletContractBuilder().walletContract(with: try wallet.publicKey, contractVersion: wallet.contractVersion)
+            async let balanceTask = balanceService.loadWalletBalance(wallet: wallet)
+            async let ratesTask = ratesService.loadRates(tonInfo: TonInfo(),
+                                                         tokens: [],
+                                                         currencies: [.USD])
+            
+            let balance = try await balanceTask
+            let formattedFiatBalance: String
+            if let rates = try? await ratesTask,
+               let tonUSDRate = rates.ton.first(where: { $0.currency == .USD }) {
+                let fiatAmount = RateConverter().convert(
+                    amount: balance.tonBalance.amount.quantity,
+                    amountFractionLength: balance.tonBalance.amount.tonInfo.fractionDigits,
+                    rate: tonUSDRate
+                )
+                formattedFiatBalance = amountFormatter.formatAmountWithoutFractionIfThousand(
+                    fiatAmount.amount,
+                    fractionDigits: fiatAmount.fractionLength,
+                    maximumFractionDigits: 2,
+                    symbol: Currency.USD.symbol
+                )
+            } else {
+                formattedFiatBalance = "\(Currency.USD.symbol ?? "")-----"
+            }
+            let formattedBalance = amountFormatter.formatAmount(
+                BigInt(integerLiteral: balance.tonBalance.amount.quantity),
+                fractionDigits: balance.tonBalance.amount.tonInfo.fractionDigits,
+                maximumFractionDigits: 2
+            )
+            return Model(
+                tonBalance: formattedBalance,
+                fiatBalance: formattedFiatBalance,
+                address: try contract.address().shortString
+            )
+        } catch {
+            throw Error.failedToLoad
+        }
     }
 }
