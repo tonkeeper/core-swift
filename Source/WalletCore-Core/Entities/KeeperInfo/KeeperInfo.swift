d4d08505cb2adeaa4a07c582e50bae22185d1351
Grigory Serebryanyy <grishamsc@gmail.com>
2023-11-18 14:16:42 +0100

    WalletCore-Core
    - add support old KeeperInfo structure in decoding

--- Source/WalletCore-Core/Entities/KeeperInfo/KeeperInfo.swift
+++ Source/WalletCore-Core/Entities/KeeperInfo/KeeperInfo.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 /// Represents the entire state of the application install
-public struct KeeperInfo: Codable {
+public struct KeeperInfo {
     /// Keeper contains multiple wallets
     let wallets: [Wallet]
     
@@ -21,4 +21,29 @@ public struct KeeperInfo: Codable {
     ///
     let assetsPolicy: AssetsPolicy
     let appCollection: AppCollection
+}
+
+extension KeeperInfo: Codable {
+    enum CodingKeys: String, CodingKey {
+        case wallets
+        case currentWallet
+        case securitySettings
+        case assetsPolicy
+        case appCollection
+    }
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        wallets = try container.decode([Wallet].self, forKey: .wallets)
+        securitySettings = try container.decode(SecuritySettings.self, forKey: .securitySettings)
+        assetsPolicy = try container.decode(AssetsPolicy.self, forKey: .assetsPolicy)
+        appCollection = try container.decode(AppCollection.self, forKey: .appCollection)
+        
+        if let currentWallet = try? container.decode(WalletIdentity.self, forKey: .currentWallet) {
+            self.currentWallet = currentWallet
+        } else {
+            let currentWallet = try container.decode(Wallet.self, forKey: .currentWallet)
+            self.currentWallet = currentWallet.identity
+        }
+    }
 }
