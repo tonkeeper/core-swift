865f71869966fa00f6e0f450938886bbcbc755e1
Grigory Serebryanyy <grishamsc@gmail.com>
2023-11-20 06:40:42 +0100

    WalletCore-Core
    - add Keychain

---
+++ Source/WalletCore-Core/Storage/Keychain/KeychainQueryable.swift
@@ -0,0 +1,12 @@
+//
+//  KeychainQueryable.swift
+//
+//
+//  Created by Grigory Serebryanyy on 20.11.2023.
+//
+
+import Foundation
+
+protocol KeychainQueryable {
+    var query: [String: AnyObject] { get throws }
+}
---
+++ Source/WalletCore-Core/Storage/Keychain/KeychainResult.swift
@@ -0,0 +1,13 @@
+//
+//  KeychainResult.swift
+//
+//
+//  Created by Grigory Serebryanyy on 20.11.2023.
+//
+
+import Foundation
+
+struct KeychainResult {
+    let status: OSStatus
+    let result: AnyObject?
+}
